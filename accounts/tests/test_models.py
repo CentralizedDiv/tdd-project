48e8dd9320f922b2c7311b016669f05b2dbbbbb7
Arthur Melo <arthurhsmelo@gmail.com>
2022-01-31 19:33:37 -0300

    Modelo Token

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,7 +1,14 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from accounts.models import Token
 
 User = get_user_model()
+
+class TokenModelTest(TestCase):
+  def test_links_user_with_auto_generated_uid(self):
+    token1 = Token.objects.create(email='a@b.com')
+    token2 = Token.objects.create(email='a@b.com')
+    self.assertNotEqual(token1.uid, token2.uid)
 
 class UserModelTest(TestCase):
   def test_user_is_valid_with_email_only(self):
